43b52f6ee3bb53027b351405accd5ccf83aeb1b9
TACJu <heju@pku.edu.cn>
2018-12-13 20:14:20 +0800

    able to train and inference as mrr now

--- code/inference.py
+++ code/inference.py
@@ -4,6 +4,8 @@ import keras
 from keras import backend as K
 from keras.models import load_model
 
+#os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+
 def MRR(pred, label):
     global num
     Q = len(num)
@@ -13,16 +15,12 @@ def MRR(pred, label):
         tmp_pred = pred[index:index + i]
         tmp_label = label[index:index + i]
         true_index = np.argmax(tmp_label)
-        tmp_pred = np.argsort(-tmp_pred)
-        rank = np.argwhere(tmp_pred == true_index)[0] + 1
+        tmp_rank = np.argsort(-tmp_pred)
+        rank = np.argwhere(tmp_rank == true_index)[0][0] + 1
         mrr += 1/rank
         index += i
     mrr /= Q
     return mrr
-    
-#os.environ["CUDA_VISIBLE_DEVICES"] = "2"
-
-#model = load_model('../model/model_01-0.96.hdf5')
 
 X_val_Q = np.load('../data/numpy_array/validation_Q_index.npy')
 X_val_A = np.load('../data/numpy_array/validation_A_index.npy')
@@ -47,43 +45,29 @@ for i in range(length):
 num = np.array(num)    
 print(len(num))
 
-count = 0
-for i in num:
-    count += i
-
-print(count)
+X_val_Q = X_val_Q[:,:200]
+X_val_A = X_val_A[:,:200]
 
 X_val = np.concatenate((X_val_Q, X_val_A), axis=1)
 Y_val = np.load('../data/numpy_array/validation_label.npy')
-print(len(Y_val))
 
 count = 0
 zero_count = 0
 for i in range(len(Y_val)):
     if Y_val[i] == 1:
         count += 1
-    elif Y_val[i] == 0:
-        zero_count += 1
     else:
-        print(Y_val[i])
+        zero_count += 1
+print(count, zero_count)
 
-'''
-result = np.zeros((len(X_val)))
-result = np.round(model.predict(X_val, 100))
+model_list = os.listdir('../model/net_model')
 
-tp = 0
-tn = 0
-fp = 0
-fn = 0
+for i in model_list:
+    model_name = '../model/net_model/' + i
+    model = load_model(model_name)
 
-for i in range(len(Y_val)):
-    if Y_val[i] == 1 and result[i] == 1:
-        tp += 1
-    elif Y_val[i] == 1 and result[i] == 0:
-        fn += 1
-    elif Y_val[i] == 0 and result[i] == 1:
-        fp += 1
-    elif Y_val[i] == 0 and result[i] == 0:
-        tn += 1
-print(tp, tn, fp, fn)
-'''
+    result = np.array(model.predict(X_val, 100))
+    result = result.reshape(result.shape[0])
+
+    mrr = MRR(result, Y_val)
+    print(mrr)
